1a31b456b581a202ccfe76fb82c01084b132880b
xwp <xwp_fullstack@163.com>
2016-10-21 07:28:23 +0800

    updae

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 print('python is so magic')
 print('python is so magic')
+print('python is so magic')
+print('python is so magic')
 print('python is itcast')
 print('python is itcast')
 print('python is itcast')
